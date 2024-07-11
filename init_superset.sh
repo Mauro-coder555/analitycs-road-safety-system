952a11c90dac0fff3b327409fdc1ae43fb5bb1ec
Mauro <maupereyra96@gmail.com>
2024-07-11 19:56:41 -0300

    fixed names superset init

--- init_superset.sh
+++ init_superset.sh
@@ -2,5 +2,20 @@
 pip install gevent
 superset db upgrade
 superset init
-superset import-dashboards -p /app/superset_exports
+
+EXPORT_PATH="/app/superset_exports"
+USERNAME="admin"  # Cambia 'admin' por el nombre de usuario correcto
+
+if [ -f "$EXPORT_PATH" ]; then
+    echo "Importando dashboards desde $EXPORT_PATH..."
+    superset import-dashboards -p "$EXPORT_PATH" -u "$USERNAME"
+    if [ $? -eq 0 ]; then
+        echo "Dashboard importado exitosamente."
+    else
+        echo "Error al importar el dashboard."
+    fi
+else
+    echo "Archivo de exportación $EXPORT_PATH no encontrado."
+fi
+
 gunicorn -w 2 -k gevent --timeout 60 -b 0.0.0.0:8088 "superset.app:create_app()"
